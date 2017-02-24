aacf0318e3eb923a7c9e45402c264ffb649c4112
wsaicyj <wsaicyj@gmail.com>
2017-02-24 18:06:13 +0800

    spider-02

--- spiders/testSpider.py
+++ spiders/testSpider.py
@@ -9,4 +9,21 @@ r = requests.get(web_url,headers=headers)
 all_a = BeautifulSoup(r.text,'lxml').find_all('a',class_='cV68d')
 # print(all_a)
 for a in all_a:
-    print(a['style'])
+    # print(a['style'])
+    img_str = a['style']
+    # print(img_str.index('"'))
+    right = img_str.index('(') + 2
+    left = img_str.index(')') - 1
+    img = img_str[right:left]
+    print('相片URL：',img)
+
+
+class BeautifulPicture():
+    def __init__(self):
+        self.headers =  {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.108 Safari/537.36'}
+        self.web_rul = 'https://unsplash.com'
+        self.save_folder = 'D:\\unloadImg'
+
+    def request(self,url):
+        re = requests.get(url)
+        return re
